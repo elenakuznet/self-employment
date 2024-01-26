ab3c0c294fc32d7fbfe9e9162213fc50ab67d503
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-01-26 10:38:30 +0500

    fix accordion

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// маска
+
+jQuery(function($){
+    $("#tel").mask("+7 (999) 999-9999");
+});
+
+// $(document).ready(function() {
+//     $("#tel").mask("+7 (999) 999-9999");
+// })
+
+
+
+
+//отослать форму
+
+const form = $('.form__content');
+
+form.submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        type: 'POST',
+        data: $(this).serialize(),
+        success(data) {
+            const [{name, tel, email}] = form;
+            $('.message').text('Спасибо, ваша заявка принята!')
+            // form.slideUp(300, function() {
+            //     tel.value = '';
+            //     email.value = '';
+            // }); 
+            console.log(data);
+            $('.name').val('');
+            $('.mobile').val('');
+            $('.email').val('');
+        }
+    })
+})
